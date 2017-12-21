42a2bd4503227a288b2faba38ae9fb3319208e5d
Tesla <anskkulgod@gmail.com>
2017-12-21 11:39:23 +0530

    DTMF- FFT
    Decodes using FFT. Works for a minimum mark and space time of 20 milliseconds respectively

---
+++ find_num.m
@@ -0,0 +1,8 @@
+function [ num ] = find_num( r,c )
+%FIND_NUM Summary of this function goes here
+%   Detailed explanation goes here
+num_array = [1,2,3;4,5,6;7,8,9;0,0,0];
+num = num_array(r,c);
+
+end
+
